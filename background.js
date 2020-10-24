95d33e44b5f8e8dac5eb30ec05aa984f66c6b843
Liz Phillips <lizkristinaphillips@gmail.com>
2020-10-24 12:09:44 -0400

    feat: run onStart script when page loaded

--- background.js
+++ background.js
@@ -15,3 +15,14 @@ chrome.runtime.onInstalled.addListener(function () {
     ]);
   });
 });
+
+chrome.tabs.onUpdated.addListener(
+  function ( tabId, changeInfo, tab )
+  { 
+    if ( changeInfo.status === "complete" )
+    {
+      chrome.tabs.executeScript(tabId, {
+      file: "/scripts/onStart.js"
+    });
+  }
+});
