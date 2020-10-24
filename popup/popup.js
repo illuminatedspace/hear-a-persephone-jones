10d334525c9c96e6e7743cb6c870b12050b03edc
Liz Phillips <lizkristinaphillips@gmail.com>
2020-10-24 12:56:06 -0400

    feat: update volume slider to diplay correct current volume

--- popup/popup.js
+++ popup/popup.js
@@ -3,6 +3,18 @@ let pauseButton = document.getElementById("pause");
 let debugButton = document.getElementById("debug");
 let volumeSlider = document.getElementById("volume");
 
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {GET_PJ_VOLUME: true}, function(response) {
+    console.log({response})
+    if(!response.PJ_VOLUME){
+      console.error("Volume Not Gotten");
+    } else {
+      console.log("Volume get successful", response.PJ_VOLUME);
+      volumeSlider.value = response.PJ_VOLUME;
+    }
+  });
+});
+
 pauseButton.onclick = function (element) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.executeScript(tabs[0].id, {
@@ -30,8 +42,8 @@ debugButton.onclick = function (element) {
 volumeSlider.onchange = function (event) {
   const volume = event.target.value;
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {PJ_VOLUME: volume}, function(response) {
-      if(!response.PJ_VOLUME_UPDATED){
+    chrome.tabs.sendMessage(tabs[0].id, {SET_PJ_VOLUME: volume}, function(response) {
+      if(!response.PJ_VOLUME){
         console.error("Volume Not Updated");
       } else {
         console.log("Volume update successful!");
--- scripts/onStart.js
+++ scripts/onStart.js
@@ -2,9 +2,10 @@ console.log("Script Running")
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     console.log({request, sender, sendResponse});
-    if (request.PJ_VOLUME) {
-        console.log("VOLUME MESSAGE", request.PJ_VOLUME);
-        window.PJ_VOLUME = request.PJ_VOLUME;
+    if (request.SET_PJ_VOLUME) {
+        window.PJ_VOLUME = request.SET_PJ_VOLUME;
+        sendResponse({PJ_VOLUME: window.PJ_VOLUME});
+    } if(request.GET_PJ_VOLUME) {
+        sendResponse({PJ_VOLUME: window.PJ_VOLUME});
     }
-    sendResponse({PJ_VOLUME_UPDATED: true});
-})
+});
