ed04819f225c6f3a7c30e00bb4ef4e7663e5e6e2
Liz Phillips <lizkristinaphillips@gmail.com>
2020-10-24 12:35:49 -0400

    feature: volume slider working

--- popup/popup.js
+++ popup/popup.js
@@ -27,8 +27,8 @@ debugButton.onclick = function (element) {
   });
 };
 
-volumeSlider.onchange = function (element) {
-  console.log('ONCHANGE', element)
+volumeSlider.onchange = function (event) {
+  const volume = event.target.value;
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {PJ_VOLUME: volume}, function(response) {
       if(!response.PJ_VOLUME_UPDATED){
--- scripts/start.js
+++ scripts/start.js
@@ -3,8 +3,11 @@ var lastLog = "";
 function playByPlay() {
   var logText = document.querySelector(".GameWidget-Log-Content").innerText;
   if (logText !== lastLog) {
+    // get volume off window set by message event listener
+    var volume = window.PJ_VOLUME || 100;
     var synth = window.speechSynthesis;
     var utterance = new SpeechSynthesisUtterance(logText);
+    utterance.volume = volume * .01;
     synth.speak(utterance);
     lastLog = logText;
   }
