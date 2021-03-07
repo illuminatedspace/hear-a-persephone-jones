53391bd1100b7be01c4a798ddef6b4722b2d38ac
Liz Phillips <lizkristinaphillips@gmail.com>
2021-03-06 21:06:16 -0500

    fix: fix debug mode

--- scripts/debug.js
+++ scripts/debug.js
@@ -2,17 +2,23 @@ console.log("DEBUG");
 function modifyGameLog() {
   // need to add conditional to check and see if one exists
     var mainNode = document.querySelector(".Main-Body");
+    var gameWidgetNode = document.querySelector(".GameWidget");
+    var widgetLogLineNode = document.querySelector(".Widget-Log-Line");
 
-    var gameWidgetNode = document.createElement("div");
-    gameWidgetNode.className = "GameWidget";
+    if (!gameWidgetNode) {
+      gameWidgetNode = document.createElement("div");
+      gameWidgetNode.className = "GameWidget";
 
-    var widgetLogLine = document.createElement("div")
-    widgetLogLine.className = "Widget-Log-Line"
+      if (!widgetLogLineNode) {
+        widgetLogLineNode = document.createElement("div")
+        widgetLogLineNode.className = "Widget-Log-Line"
+        gameWidgetNode.appendChild(widgetLogLineNode);
+      }
 
-    gameWidgetNode.appendChild(widgetLogLine);
-    mainNode.appendChild(gameWidgetNode);
+      mainNode.appendChild(gameWidgetNode);
+    } 
 
-    widgetLogLine.innerText = Math.ceil(Math.random() * 10);
+    widgetLogLineNode.innerText = Math.ceil(Math.random() * 10);
   window.setTimeout(function () {
     modifyGameLog();
   }, 3000);
